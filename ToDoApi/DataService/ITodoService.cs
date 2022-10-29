c893e2cc15342aba70b1c781cdf174eb5dc91dbe
Haman shareghi <haman.shareghi@outlook.com>
2022-10-29 19:31:58 +0400

    Add data Service And Register In Program cs

--- ToDoApi/DataService/ITodoService.cs
+++ ToDoApi/DataService/ITodoService.cs
@@ -1,7 +1,13 @@
-﻿namespace ToDoApi.DataService
+﻿using ToDoApi.Models;
+
+namespace ToDoApi.DataService
 {
     public interface ITodoService
     {
-        
+        IEnumerable<ToDo> GetAll();
+        ToDo GetById(int id);
+        void Add(ToDo todo);
+        void Update(ToDo todo);
+        void Delete(int id);
     }
 }
--- ToDoApi/DataService/ToDoService.cs
+++ ToDoApi/DataService/ToDoService.cs
@@ -1,6 +1,56 @@
-﻿namespace ToDoApi.DataService
+﻿using System.Diagnostics;
+using ToDoApi.Data;
+using ToDoApi.Models;
+
+namespace ToDoApi.DataService
 {
     public class ToDoService : ITodoService
     {
+        private AppDbContext _context;
+
+        public ToDoService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<ToDo> GetAll()
+        {
+            var data = _context.ToDo
+                .ToList();
+            return data;
+        }
+
+        public ToDo GetById(int id)
+        {
+            var data = _context.ToDo
+                .FirstOrDefault(s => s.Id == id);
+            if(data == null)
+                Debug.WriteLine("-- Not Found --");
+            return data;
+
+        }
+
+        public void Add(ToDo todo)
+        {
+            _context.ToDo.Add(todo);
+            _context.SaveChanges();
+
+        }
+
+        public void Update(ToDo todo)
+        {
+            _context.ToDo.Update(todo);
+            _context.SaveChanges();
+
+        }
+
+        public void Delete(int id)
+        {
+            var data = _context.ToDo
+                .FirstOrDefault(s => s.Id == id);
+            if (data == null)
+                Debug.WriteLine("-- Not Found --");
+            _context.ToDo.Remove(data);
+            _context.SaveChanges();
+        }
     }
 }
